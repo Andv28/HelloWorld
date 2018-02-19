5ba4455271c90059473279360402b96d7d7b4915
Villalobos Palma <aiv251@email.vccs.edu>
2018-02-19 14:21:01 -0500

    message

---
+++ Information.java
@@ -0,0 +1,8 @@
+public class information  {
+	public static class void main (String [] args){
+		System.out.println("Andrea");
+		System.out.println("1/22/2018");
+		System.out.println("1/23/2018");
+		System.out.println("1/21/2018");
+	}
+}
